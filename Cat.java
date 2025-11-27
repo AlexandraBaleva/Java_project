3ff38f59109ca8f8506960e14200b7aa60381b1e
AlexandraBaleva <alexsandra3008@abv.bg>
2025-11-27 08:57:50 +0200

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,27 @@
+public class Cat {
+    int age;
+    String name;
+    String color; 
+
+public static void main(String[] args) {
+    Cat myCat = new Cat();
+    myCat.name = "Jordan";
+    System.out.println("Machkata se kazva " + myCat.name);
+}
+
+
+
+public Cat(){
+
+}
+
+
+public Cat(int age, String name, String color){
+    this.age = age;
+    this.name = name;
+    this.color = color;
+
+ }
+
+  }
+
